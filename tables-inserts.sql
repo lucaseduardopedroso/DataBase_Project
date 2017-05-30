4e798b61ebdf5be32ae7f386b6802821ccd5130a
lucaseduardopedroso <lucaseduardosilvapedroso@gmail.com>
2017-05-30 19:06:54 -0300

    Update tables-inserts.sql

--- tables-inserts.sql
+++ tables-inserts.sql
@@ -4,11 +4,30 @@ DROP TABLE IF EXISTS FORNECEDOR;
 DROP TABLE IF EXISTS FUNCIONARIO_RESIDE;
 
 CREATE TABLE FUNCIONARIO (Id_Func INTEGER, Nome_Func (100),
-                          Data_Nasc DATE, Sexo CHAR (10), 
-                          CPF INTEGER, Cargo CHAR (20),
+                          F_Telefone INTEGER, F_Email CHAR (50),
+                          F_Data_Nasc DATE, F_Sexo CHAR (10), 
+                          F_CPF INTEGER, Cargo CHAR (20),
              PRIMARY KEY (Id_Func)
                           
 );
+
+CREATE TABLE CLIENTE (Id_Cliente INTEGER, Nome_Cliente (100),
+                      C_Data_Nasc DATE, C_Sexo CHAR (10), 
+                      C_CPF INTEGER, C_Email CHAR (50),
+                      C_Telefone INTEGER,
+             PRIMARY KEY (Id_Cliente)
+                      
+);
+
+CREATE SESSAO (Cod_Sessao INTEGER, 
+               HorarioInicial TIME, HorarioFinal TIME,
+               Id_Filme INTEGER, Nome_Filme CHAR (100),
+       FOREIGN KEY (Id_Filme) REFERENCES FILME (Id_Filme),
+       FOREIGN KEY (Nome_Filme) REFERENCES FILME (Nome_Filme),
+       PRIMARY KEY (Cod_Sessao)
+               
+);        
+               
 
 CREATE TABLE FUNCIONARIO_RESIDE(Cidade CHAR (20), UF CHAR (2),
                                 Numero INTEGER, CEP INTEGER,
@@ -19,8 +38,8 @@ CREATE TABLE FUNCIONARIO_RESIDE(Cidade CHAR (20), UF CHAR (2),
 );
 
 CREATE TABLE FORNECEDOR (Id_Forn INTEGER, Nome_Forn CHAR (50),
-                         CNPJ INTEGER, Telefone INTEGER,
-                         Email CHAR (50),
+                         CNPJ INTEGER, FO_Telefone INTEGER,
+                         FO_Email CHAR (50),
              PRIMARY KEY (Id_Forn)            
 );
 
