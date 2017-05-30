1b8afddcbf493d886ad35f9f1a369d4af09fdfc4
ThaNakao <Thaisekaori@hotmail.com>
2017-05-30 18:15:43 -0300

    Update tables-inserts.sql

--- tables-inserts.sql
+++ tables-inserts.sql
@@ -8,11 +8,12 @@ CREATE TABLE FILME (Id_Filme INTEGER, Nome_Filme CHAR (30),
 ); 
 
 CREATE TABLE FUNCIONARIO (Nome_Func CHAR (30), Id_Func INTEGER,
-                          Cargo CHAR (20)     
+                          CPF INTEGER, Cargo CHAR (20),     
                           
 );
 
 CREATE TABLE FORNECEDOR (Nome_Forn CHAR (30), Id_Forn INTEGER,
+                         CPF INTEGER, 
                          
 );
 
