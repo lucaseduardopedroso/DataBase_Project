625fd2a341c8743595692f6300057aaf342fa420
lucaseduardopedroso <lucaseduardosilvapedroso@gmail.com>
2017-05-30 18:34:53 -0300

    Update tables-inserts.sql

--- tables-inserts.sql
+++ tables-inserts.sql
@@ -2,23 +2,32 @@ DROP TABLE IF EXISTS FILME;
 DROP TABLE IF EXISTS FUNCIONARIO;
 DROP TABLE IF EXISTS FORNECEDOR;
 
-
-CREATE TABLE FILME (Id_Filme INTEGER, Nome_Filme CHAR (30), 
-                    Faixa_E INTEGER,
-                    Sinopse CHAR (500), Diretor CHAR (30),
-                    Elenco CHAR (140), Genero CHAR (20),
-                    Duracao TIME, Idioma CHAR (30)                    
-); 
-
-CREATE TABLE FUNCIONARIO (Nome_Func CHAR (30), Id_Func INTEGER,
-                          CPF INTEGER, Cargo CHAR (20),     
+CREATE TABLE FUNCIONARIO (Id_Func INTEGER, Nome_Func (100),
+                          Data_Nasc DATE, Sexo CHAR (10), 
+                          CPF INTEGER, Cargo CHAR (20),
+             PRIMARY KEY (Id_Func)
                           
 );
 
-CREATE TABLE FORNECEDOR (Nome_Forn CHAR (30), Id_Forn INTEGER,
-                         CPF INTEGER, 
-                         
+CREATE TABLE FUNCIONARIO_RESIDE(
+  
 );
+
+CREATE TABLE FORNECEDOR (Id_Forn INTEGER, Nome_Forn CHAR (50),
+                         CNPJ INTEGER, Telefone INTEGER,
+                         Email CHAR (50),
+             PRIMARY KEY (Id_Forn)            
+);
+
+CREATE TABLE FILME (Id_Filme INTEGER, Nome_Filme CHAR (100), 
+                    FaixaEtaria INTEGER,
+                    Sinopse CHAR (500), Diretor CHAR (30),
+                    Elenco CHAR (140), Genero CHAR (20),
+                    Duracao TIME, Idioma CHAR (30),
+                    Id_Forn INTEGER,
+             PRIMARY KEY (Id_Filme),
+             FOREIGN KEY (Id_Forn) REFERENCES FUNCIONARIOS (Id_Forn)
+); 
 
 
 INSERT INTO VALUES 
