61711d1808ea7e3e44808dd6e9f96912a7acddc1
lucaseduardopedroso <lucaseduardosilvapedroso@gmail.com>
2017-05-30 13:35:27 -0300

    Update tables-inserts.sql

--- tables-inserts.sql
+++ tables-inserts.sql
@@ -1,6 +1,8 @@
 DROP TABLE IF EXISTS;
 
-CREATE TABLE ( 	
+CREATE TABLE PRODUTO (Nome_Produto CHAR (30), Id_Produto INTEGER,
+                      Desc_Produto CHAR (140),
+                      Preco REAL,  	
 ); 
 
 INSERT INTO VALUES 
