f1927d224bea2053459b51ea808b5895c1c36c63
ThaNakao <Thaisekaori@hotmail.com>
2017-05-30 16:32:43 -0300

    Update tables-inserts.sql

--- tables-inserts.sql
+++ tables-inserts.sql
@@ -1,8 +1,18 @@
 DROP TABLE IF EXISTS;
 
-CREATE TABLE PRODUTO (Nome_Produto CHAR (30), Id_Produto INTEGER,
-                      Desc_Produto CHAR (140),
-                      Preco REAL,  	
+CREATE TABLE FILME (Nome_Filme CHAR (30), Faixa_E INTEGER,
+                      Sinopse CHAR (140), 
+                    
 ); 
+
+CREATE TABLE FUNCIONARIO (Nome_Func CHAR (30), Id_Func INTEGER,
+                          Cargo CHAR (20)     
+                          
+);
+
+CREATE TABLE FORNECEDOR (Nome_Forn CHAR (30), Id_Forn INTEGER,
+                         
+);
+
 
 INSERT INTO VALUES 
