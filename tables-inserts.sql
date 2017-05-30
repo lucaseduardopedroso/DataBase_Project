1ee1332ab32e2ecf69bfde5d26e718f986f59a78
lucaseduardopedroso <lucaseduardosilvapedroso@gmail.com>
2017-05-30 18:12:40 -0300

    Update tables-inserts.sql

--- tables-inserts.sql
+++ tables-inserts.sql
@@ -1,8 +1,10 @@
 DROP TABLE IF EXISTS;
 
-CREATE TABLE FILME (Nome_Filme CHAR (30), Faixa_E INTEGER,
-                      Sinopse CHAR (140), 
-                    
+CREATE TABLE FILME (Id_Filme INTEGER, Nome_Filme CHAR (30), 
+                    Faixa_E INTEGER,
+                    Sinopse CHAR (500), Diretor CHAR (30),
+                    Elenco CHAR (140), Genero CHAR (20),
+                    Duracao TIME, Idioma CHAR (30)                    
 ); 
 
 CREATE TABLE FUNCIONARIO (Nome_Func CHAR (30), Id_Func INTEGER,
